decf2e5fe8fdc8ed99a8c9678e8fcee5bf711c2e
FatihG34 <fthgnydn34@gmail.com>
2022-05-20 22:41:42 +0300

    working js

---
+++ 002-Assignment-js.js
@@ -0,0 +1,26 @@
+let email = "asdf@gmail.com";
+console.log(email);
+
+var a = ['dog', 'lion', 'hen'];
+console.log(a.length);
+a[100] = 'horse';
+console.log(a.length);
+console.log(typeof a[50]);
+console.log(a);
+let b;
+console.log(b);
+function addTax(total) {
+    return total * 1.05;
+}
+;
+console.log(addTax(50));
+
+let price = (price) => {
+    return price * 0.85;
+};
+console.log(price);
+
+
+"use strict";
+x = 3.14;
+console.log(x);
