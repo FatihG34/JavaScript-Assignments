3fd29aed418b7da6442edf3bdce22e7cca120ffe
FatihG34 <fthgnydn34@gmail.com>
2022-07-14 17:19:44 +0300

    description added

--- 007-Assignment-js.js
+++ 007-Assignment-js.js
@@ -1,3 +1,11 @@
+//* Time to put your newly learned skills to work!
+
+//* This is an interview question asked by Google.drawing
+
+//* Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+
+
+
 const array = [10, 5, 2, 7, 8, 7, 22, 9, 15];
 let k = 5;
 console.log('Length of Given Array :', array.length);
