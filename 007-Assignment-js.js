8108b77631131cf673cda76b032b651697c92c03
FatihG34 <fthgnydn34@gmail.com>
2022-07-15 22:31:14 +0300

    upgrated

--- 007-Assignment-js.js
+++ 007-Assignment-js.js
@@ -5,34 +5,12 @@
 //* Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
 
 
-
-// const array = [1, 2, 3, 4, 5, 6, 7];
-// let k = 3;
-// console.log('Length of Given Array :', array.length);
-
-// const slidingMax = (arr, k) => {
-//     if (k < 1 || k > arr.length) {
-//         alert(`${k} must bu greater than 1 and less than ${arr.length}`)
-//     }
-//     const newArray = [];
-//     for (let i = 0; i < arr.length - k + 1; i++) {
-//         if () {
-//             newArray.push(Math.max(arr[i], arr[i + 1], arr[i + 2]))
-//         }
-
-//     }
-//     console.log(newArray);
-// }
-// slidingMax(array, k);
-
-
-
-
-
-
 function printKMax(arr, k) {
+    if (k < 1 || k > arr.length) {
+        alert(`${k} must bu greater than 1 and less than ${arr.length}`)
+    }
     let j, max;
-    let newList = [];
+    const newList = [];
 
     for (let i = 0; i <= arr.length - k; i++) {
         max = arr[i];
@@ -44,12 +22,12 @@ function printKMax(arr, k) {
         newList.push(max)
     }
     console.log(newList)
-}
+};
 
-
-let arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const aArray = [1, 3, -1, -3, 5, 3, 6, 7];
 let k = 3;
-printKMax(arrr, k);
-const newArr = [22, 5, 23, 12, 77, 52, 14, 83];
+printKMax(aArray, k);
+
+const newArr = [22, 5, 23, 12, 52, 2, 14, 23];
 let num = 4;
 printKMax(newArr, num)
